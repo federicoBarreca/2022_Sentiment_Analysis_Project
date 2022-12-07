70cd7e689450c0879624e14b32ad47b09392b347
Federico Barreca <federico.barreca97@gmail.com>
2022-12-08 00:50:07 +0100

    update

--- FDS_project_2022/FDS_project_2022/repo/functions.py
+++ FDS_project_2022/FDS_project_2022/repo/functions.py
@@ -3,11 +3,11 @@ from joblib import dump, load
 
 def testFunction(x):
 
-    #dump(cv, 'FDS_project_2022/repo/CountVectorizer.joblib')
+    #dump(cv, 'FDS_project_2022/repo/lrCountVectorizer.joblib')
     #dump(lrModel, 'FDS_project_2022/repo/lrTrainedModel.joblib')
 
     lrModel = load('FDS_project_2022/repo/lrTrainedModel.joblib')
-    cv = load('FDS_project_2022/repo/CountVectorizer.joblib')
+    cv = load('FDS_project_2022/repo/lrCountVectorizer.joblib')
     x = cv.transform([x])
 
     res = lrModel.predict(x)

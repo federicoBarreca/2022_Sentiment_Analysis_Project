7db6a63fdaed404e05fa507c24a0f65268e5846f
Federico Barreca <federico.barreca97@gmail.com>
2022-12-08 00:48:04 +0100

    Modifiche librerie e modello

--- FDS_project_2022/FDS_project_2022/repo/functions.py
+++ FDS_project_2022/FDS_project_2022/repo/functions.py
@@ -1,33 +1,15 @@
-import numpy as np # imports a fast numerical programming library
-import scipy as sp # imports stats functions, amongst other things
-#import matplotlib as mpl # this actually imports matplotlib
-#import matplotlib.cm as cm # allows us easy access to colormaps
-#import matplotlib.pyplot as plt # sets up plotting under plt
-#import pandas as pd # lets us handle data as dataframes
-#from sklearn.datasets import make_classification
-#import seaborn as sns
-#from tqdm import tqdm
+from joblib import dump, load
 
 
 def testFunction(x):
-    '''
-    Function to compute the sigmoid of a given input x.
+
+    #dump(cv, 'FDS_project_2022/repo/CountVectorizer.joblib')
+    #dump(lrModel, 'FDS_project_2022/repo/lrTrainedModel.joblib')
+
+    lrModel = load('FDS_project_2022/repo/lrTrainedModel.joblib')
+    cv = load('FDS_project_2022/repo/CountVectorizer.joblib')
+    x = cv.transform([x])
+
+    res = lrModel.predict(x)
     
-    Input:
-    x: it's the input data matrix. The shape is (N, H)
-
-    Output:
-    g: The sigmoid of the input x
-    '''
-    
-    #g = 1/(1+np.exp(-int(x)))
-
-    g = Test(x)
-
-    return g.field
-
-class Test:
-    """description of class"""
-    field = "campo"
-    def __init__(self, campo):
-        self.field = campo
+    return res

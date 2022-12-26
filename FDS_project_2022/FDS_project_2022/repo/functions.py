7579cbde7dc18c175c653d77ce3f6d1b12468d9b
Federico Barreca <federico.barreca97@gmail.com>
2022-12-26 17:46:39 +0100

    final edits and comments

--- FDS_project_2022/FDS_project_2022/repo/functions.py
+++ FDS_project_2022/FDS_project_2022/repo/functions.py
@@ -1,11 +1,18 @@
 from joblib import load
 
+# Get a sample in input, extract the features and perform a class prediction
 def predictEmotion(x):
 
+    # Load logistic regression trained model
     lrModel = load('FDS_project_2022/repo/LogisticRegressionTrainedModel.joblib')
+    
+    # Load count vectorizer fitted for the model
     cv = load('FDS_project_2022/repo/FittedCountVectorizer.joblib')
+
+    # Features extraction
     x = cv.transform([x])
 
+    # Prediction
     res = lrModel.predict(x)
     
     return res
